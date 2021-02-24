ac2ce121038b05dc17cffd5e005fb6e5ab19cb39
Alexey Matsuhanov <manexin1@gmail.com>
2021-02-24 10:34:01 +0300

    Creating paths for storing temporary files

--- Scripting_assignments/ip-address.sh
+++ Scripting_assignments/ip-address.sh
@@ -2,26 +2,27 @@
 
 if [[ -n "$1" ]]
 then
-    ss -4platu > ~/connect
-    awk -v pattern="$1" '$0 ~ pattern {print $6}' ~/connect > ~/ipPort
-    cut -d: -f1 ~/ipPort | uniq -u > ~/ip
-    tail -n5 ~/ip > ~/last5ip
-    if [[ -s ~/last5ip  ]]
+    mydir=$(mktemp -d "${TMPDIR:-/tmp/}$(basename $0).XXXXX")
+    ss -4platu > $mydir/connect
+    awk -v pattern="$1" '$0 ~ pattern {print $6}' $mydir/connect > $mydir/ipPort
+    cut -d: -f1 $mydir/ipPort | uniq -u > $mydir/ip
+    tail -n5 $mydir/ip > $mydir/last5ip
+    if [[ -s $mydir/last5ip  ]]
     then
-        for IP in $(cat ~/last5ip)
+        for IP in $(cat $mydir/last5ip)
         do
-           whois $IP > ~/ipInfo
-           OrgCi=$(awk -F':' '/^Organization/ {print $2}/^City/ {print $2}' ~/ipInfo)
+           whois $IP > $mydir/ipInfo
+           OrgCi=$(awk -F':' '/^Organization/ {print $2} /^City/ {print $2}' $mydir/ipInfo)
            echo "$OrgCi"
         done
         if [[ -z $OrgCi ]]
            then
                echo "Organization and City is not known for this procces! See all information about the connections of process $1 ."
-               cat ~/ipInfo
+               cat $mydir/ipInfo
            else
                echo "If you want other information, edit the script!"
          fi
-        rm ~/connect ~/ipPort ~/ip ~/last5ip ~/ipInfo
+         rm -r $mydir
     else
         echo "Process not found, please try again"
     fi
