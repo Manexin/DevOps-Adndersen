b875fc3a4c14826a3e1c1ab9d0294347cb80a9b2
Alexey Matsuhanov <manexin1@gmail.com>
2021-03-03 00:36:31 +0300

    doing task2

--- Financial_genius/volatility.sh
+++ Financial_genius/volatility.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 #download JSON
-quotes=`curl -s https://yandex.ru/news/quotes/graph_2000.json`
+#quotes=`curl -s https://yandex.ru/news/quotes/graph_2000.json`
 
+mydir=$(mktemp -d "${TMPDIR:-/tmp/}$(basename $0).XXXXX")
 #create quotes table
-jr -r '.prices[][]' $quotes
-echo $quotes
-# jq -r '.prices[][0] | (./1000 | strftime("%m-%Y"))' $quote
+#jq -c '.prices' quotes.json
 
+#created new object JSON
+jq '{date: (.prices[][0] | (./1000 | strftime("%F"))), cost: .prices[][1]}' quotes.json > $mydir/new_quotes.json
+
+jq '.date' $mydir/new_quotes.json
+#echo $quotes
+#jq '.prices[][0], .prices[][1]' quotes.json 
+#qu_pri=`jq '.prices[][]' quotes.json`
+#dates=`jq -r '.prices[][0] | (./1000 | strftime(%F))' quotes.json`
+#echo $dates
+#echo ${qu_pri} | awk '{print $0, NF}'
