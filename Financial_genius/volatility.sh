d55912af4ca7b76ab04639d3374ef7f1a36a677b
Alexey Matsuhanov <manexin1@gmail.com>
2021-03-07 00:26:33 +0300

    created function true date-value

--- Financial_genius/volatility.sh
+++ Financial_genius/volatility.sh
@@ -7,73 +7,51 @@
 #FUNCTIONS
 
 get_true_dates (){
-	        NORMAL_DATE=$(jq -c '.prices[][0] |= (./1000 | strftime ("%m-%Y")) | .prices[] | .[]' quotes.json)
+	        DATE_VALUE=$(jq -j '.prices[][0] |= (./1000 | strftime (" %m:%y-")) | .prices[] | .[]' quotes.json)
+		ARR_DATE_VALUE=(`echo "$DATE_VALUE"`)
+		HUM_DATE_VALUE=$(echo "${ARR_DATE_VALUE[*]}" | sed 's/\s/\n/g' | awk -F'-' '{print $0}')
 		}
-get_currency_rate (){
-		CUR_RATE=$(jq -j '.prices[][1]' quotes.json)
+get_choosing_date (){
+		CHOOSE_DATE=$(echo "$NORMAL_DATE" | grep "0221:\n[0-9]+.[0-9]+") # ^"$pattern"20[0-9]{2})
 		}
-get_join_as (){
-		local IFS="$1"; shift; echo "$*";
-		}
-true_quote (){
-		NEW_QUOTE=
-		}
+
+
+# jq -r --arg date "$1" '.prices[][0] |= (./1000 | strftime ("%m")) | .prices[] | .[] | map(select( . == $date))' quotes.json)
+
 # WORKED IF
 #--------------------------------------
-#echo -n "Enter number of 'month':"
-#read month
-#if [ $month -ge 1 ]&&[ $month -le 12 ]
-#   then
-#     echo "The lowest volatility in the month $month was in the year:"
-#     if  [[ $month == [0-9] ]]
-#        then
-#          pattern=0$month
-#        else
-#          pattern=$month
-#	fi
-#   else
-#     echo "You enter wrong number month"
-#     exit
-#fi
+echo -n "Enter number of 'month':"
+read month
+if [ $month -ge 1 ]&&[ $month -le 12 ]
+   then
+     echo "The lowest volatility in the month $month was in the year:"
+     if  [[ $month == [0-9] ]]
+        then
+          pattern=0$month
+        else
+          pattern=$month
+	fi
+   else
+     echo "You enter wrong number month"
+     exit
+fi
 #--------------------------------------------------
+echo "$pattern"
+#jq --arg date "03-15" '.prices[][0] |= (. / 1000 | strftime("%m-%y")) | .prices[] | .[] | map(select( .prices[][0] == $date))' quotes.json
 
-#jq --arg date "03-15" '.prices[][0] |= (. / 1000 | strftime("%m-%Y")) | map(select(.prices[][0] == $date))' quotes.json
+#jq --arg date "$pattern" -r '.prices[][0] |= ( . / 1000 | strftime("%m")) | .prices[] | .[] | map(select( .[] == $date)) | .[1]' quotes.json
 
-#jq -c '.prices[][0] |= (. / 1000 | strftime("%m-%Y")) | .prices[] | .[]' quotes.json
+get_true_dates
+echo "$HUM_DATE_VALUE" | awk '/03:/ {print}'
 
-#mydir=$(mktemp -d "${TMPDIR:-/tmp/}$(basename $0).XXXXX")
+#get_choosing_date "$pattern"
+#echo "$CHOOSE_DATE"
 
-#create quotes table
+#get_choosing_date
+#echo "$CHOOSE_DATE"
 
-#jq -jr '.prices[][0] |= (. / 1000 | strftime("%m-%Y"))' quotes.json  # | jq -r '.prices[] | join("")' # | sort
+#date=$(jq -j '.prices[][0] |= (./1000 | strftime (" %m:%y-")) | .prices[] | .[]' quotes.json)
+#arr_date=(`echo "$date"`)
+#DATE_VALUE=$(echo "${arr_date[*]}" | sed 's/\s/\n/g' | awk -F'-' '{print $0}')
+#echo "$DATE_VALUE"
 
-#jq -jr '.prices[][0] |= (. / 1000 | strftime("%Y%m"))' $quotes | jq -r '.prices[]|join(" ")' # | grep 202103 | sort
-
-# created new object JSON
-# jq '{date: (.prices[][0] | (./1000 | strftime("%F"))), cost: .prices[][1]}' quotes.json > $mydir/new_quotes.json
-
-# jq '.date' $mydir/new_quotes.json
-# echo $quotes
-
-
-
-
-#get_true_dates
-# get_currency_rate
-#echo "$NORMAL_DATE"
-#echo "$CUR_RATE"
-#WORKED
-#QUOTE=$(paste <(echo "$NORMAL_DATE") <(echo "$CUR_RATE"))
-#echo "$QUOTE"
-
-#NOT WORKED
-#test=$(xargs -L 1 get_join_as - "$NORMAL_DATE" "$CUR_RATE")
-#echo -e "$test"
-#get_join_as - t e   -- ''s"" t i n g 
-
-#WORKED FOR
-#for new_dates in $dates;
-#do
-#tr_dates+=$(date --date=@$new_dates "+%m:%y-%t")
-#done
-# echo  "$tr_dates"
