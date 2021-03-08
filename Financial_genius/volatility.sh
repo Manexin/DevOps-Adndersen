6d7406dc3e0752baf4ae5823eda797eb680c6b72
Alexey Matsuhanov <manexin1@gmail.com>
2021-03-08 15:41:20 +0300

    get min volatility of all year

--- Financial_genius/volatility.sh
+++ Financial_genius/volatility.sh
@@ -15,9 +15,15 @@ get_choosing_month (){
 		CHOOSE_MONTH=$(echo "$HUM_DATE_VALUE" | grep "$pattern:..")
 		}
 
-get_volatility_month (){
-		VOL_MONTH=
-		}
+#get_volatility_month (){
+#		VOL_MONTH=(`for (( i="$min_y"; i<"$max_y"; i++ ))
+#        do
+#                min_y=$(( $min_y + 1 ))
+#                value_year=(`echo "$CHOOSE_MONTH" | cut -d':' -f2 | awk  -F'-' -v year="$i" '$1 ~ year {print $2}' | sort -n`)
+#                volatility_year=$(echo "scale=4; (${value_year[-1]}-${value_year[0]})/2" | bc -l)
+#                echo "$volatility_year"
+#	done`)
+#		}
 # jq -r --arg date "$1" '.prices[][0] |= (./1000 | strftime ("%m")) | .prices[] | .[] | map(select( . == $date))' quotes.json)
 
 # WORKED IF
@@ -46,38 +52,38 @@ get_choosing_month
 min_y=15
 max_y=$(date +"%y")
 
-
-for (( i="$min_y"; i<"$max_y"; i++ ))
-do
-
-min_y=$(( $min_y + 1 ))
-value_year=(`echo "$CHOOSE_MONTH" |cut -d':' -f2 | awk  -F'-' -v year="$i" '$1 ~ year {print $2}' | sort -n`)
-volatility_year=$(echo "scale=4; (${value_year[-1]}-${value_year[0]})/2" | bc -l)
-echo "$volatility_year"
-
-done
-
-
-#echo "$CHOOSE_MONTH" |cut -d':' -f2 | awk  -F'-' -v year="$min_y" '$1 ~ year {print $2}' | sort -n
-#value_year=(`echo "$CHOOSE_MONTH" |cut -d':' -f2 | awk  -F'-' -v year="$min_y" '$1 ~ year {print $2}' | sort -n`)
-#volatility_year=$(echo "scale=4; (${value_year[-1]}-${value_year[0]})/2" | bc -l)
-#echo "$volatility_year"
+#WORKED FOR
+#-------------------------------------
+#for (( i="$min_y"; i<"$max_y"; i++ ))
+#	do
+#		min_y=$(( $min_y + 1 ))
+#		value_year=(`echo "$CHOOSE_MONTH" |cut -d':' -f2 | awk  -F'-' -v year="$i" '$1 ~ year {print $2}' | sort -n`)
+#		volatility_year=$(echo "scale=4; (${value_year[-1]}-${value_year[0]})/2" | bc -l)
+#		result_year=$(echo "$CHOOSE_MONTH" |cut -d':' -f2 | awk  -F'-' -v year="$i" '$1 ~ year {print
+#
+#done
+#----------------------------------------
 
 #while [ $min_y -le $max_y ]
 #do
 #min_y=$(( $min_y + 1 ))
-#value_year=(`echo "$CHOOSE_MONTH" |cut -d':' -f2 | awk  -F'-' -v year="$min_y" '$1 ~ year {print $2}' | sort -n`)
-#volatility_year=$(echo "scale=4; (${value_year[-1]}-${value_year[0]})/2" | bc -l)
-#echo "$volatility_year"
 
+for (( i="$min_y"; i<"$max_y"; i++ ))
+do
+min_y=$(( $min_y + 1 ))
+value_year=(`echo "$CHOOSE_MONTH" |cut -d':' -f2 | awk  -F'-' -v year="$i" '$1 ~ year {print $2}' | sort -n`)
+#echo -e "${value_year[*]}"
+volatility_year=$(echo "${value_year[*]}" | echo "scale=4; (${value_year[-1]}-${value_year[0]})/2" | bc -l)
+#year_vol=$(echo "$i $volatility_year") # | awk '{min=$2; for ( i=2; i<=NF; i++ ) if ($i<min) min=$i; print$2}'
+echo "$i $volatility_year"
+
+# echo "$year_vol" # | awk '{min=$2; for ( i=2; i<=NF; i++ ) if ( $i<min ) min=$i; print $0}'
+done
+
+#echo "$i $volatility_year" # | awk '{min=$2; for ( i=2; i<=NF; i++ ) if ($i<min) min=$i; print$2}'
 #done
-
-
-#get_choosing_date
-#echo "${CHOOSE_DATE[*]}"
 
 #date=$(jq -j '.prices[][0] |= (./1000 | strftime (" %m:%y-")) | .prices[] | .[]' quotes.json)
 #arr_date=(`echo "$date"`)
 #DATE_VALUE=$(echo "${arr_date[*]}" | sed 's/\s/\n/g' | awk -F'-' '{print $0}')
 #echo "$DATE_VALUE"
-
