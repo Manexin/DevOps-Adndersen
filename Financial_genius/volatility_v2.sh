b9afd0d1f955f430ca28affabdcd1382c91cfbda
Alexey Matsuhanov <manexin1@gmail.com>
2021-03-10 00:50:18 +0300

    new output

---
+++ Financial_genius/volatility_v2.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [ ! -e ./quotes.json ]
+	then #download actual JSON
+	curl -so ./quotes.json https://yandex.ru/news/quotes/graph_2000.json
+fi
+
+#FUNCTIONS
+
+get_true_dates (){
+	        DATE_VALUE=$( jq -j '.prices[][0] |= (./1000 | strftime (" %m:%Y-")) | .prices[] | .[]' ./quotes.json )
+		ARR_DATE_VALUE=(`echo "$DATE_VALUE"`)
+		HUM_DATE_VALUE=$(echo "${ARR_DATE_VALUE[*]}" | sed 's/\s/\n/g' | awk -F'-' '{print $0}')
+		}
+get_choosing_month (){
+		CHOOSE_MONTH=$(echo "$HUM_DATE_VALUE" | grep "$pattern:..")
+		}
+
+# Entering Month that are interested us.
+echo -n "Enter the 'month' you are interested in: "
+read month
+if [ $month -ge 1 ]&&[ $month -le 12 ]
+   then
+     echo "The lowest volatility in the month $month was in the year:"
+     if  [[ $month == [0-9] ]]
+        then
+          pattern=0$month
+        else
+          pattern=$month
+	fi
+   else
+     echo -e "There is no such month.\nWe could input month number from 1 to 12, example 3 or 03"
+     exit
+fi
+
+get_true_dates
+
+get_choosing_month
+
+min_y=2015
+max_y=$(date +"%Y")
+
+# We compare the volatility and show the year with the minimum volatility
+
+vol=$( for (( i="$min_y"; i<"$max_y"; i++ ))
+  do
+	min_y=$(( $min_y + 1 ))
+	# Get array value in choose month of every year
+	value_year=(`echo "$CHOOSE_MONTH" |cut -d':' -f2 | awk  -F'-' -v year="$i" '$1 ~ year {print $2}' | sort -n`)
+
+	# Get volatility in choose month of every year
+	volatility_year=$(echo "${value_year[*]}" | echo "scale=4; (${value_year[${#a[@]}-1]}-${value_year[0]})/2" | bc -l)
+	echo "$i:$volatility_year"
+# choose the year with the lowest volatility and print
+done)
+res_year=$(echo "$vol" | sort -t':' -nk2 | head -n1 | cut -d':' -f1)
+res_vol=$(echo "$vol" | sort -t':' -nk2 | head -n1 | cut -d':' -f2)
+echo -e " $res_year, volatility was $res_vol"
